a3400ac3e40c0a2395a78dcc3159457ca55b962e
Slava Shapkin <slavrikk@yandex.ru>
2020-08-03 16:07:09 +0300

    homework 1

---
+++ src/test/java/hw1/TestSumDouble.java
@@ -0,0 +1,16 @@
+package hw1;
+
+import com.epam.tat.module4.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSumDouble {
+
+
+@Test(dataProviderClass = DataProviders.class, dataProvider = "sum-double")
+    public void testSumDoubleFunctionality(double a, double b, double expectedResult){
+    Calculator calculator = new Calculator();
+    double actualResult = calculator.sum(a,b);
+    Assert.assertEquals(actualResult, expectedResult, 0.1);
+}
+}
