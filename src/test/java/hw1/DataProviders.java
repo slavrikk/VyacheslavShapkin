307844a9fb46c97b5903d91934801c81d87f2f9e
Slava Shapkin <slavrikk@yandex.ru>
2020-08-05 18:44:03 +0300

    fix DataProvider

--- src/test/java/hw1/DataProviders.java
+++ src/test/java/hw1/DataProviders.java
@@ -6,17 +6,20 @@ public class DataProviders {
 
     @DataProvider(name = "sum-double")
     public static Object[][] dataProviderMethodSumDouble() {
-        return new Object[][]{{10.5, 10.5, 21.0}, {50.5, 50.5, 101.0}, {96.5621, 98.4221, 194.9842}};
+        return new Object[][]{{10.5, 10.5, 21.0}, {50.5, 50.5, 101.0},
+                {96.5621, 98.4221, 194.9842}};
     }
 
     @DataProvider(name = "sub-double")
     public static Object[][] dataProviderMethodSubDouble() {
-        return new Object[][]{{13.53, 5.12, 8.41}, {56.789874, 25.98744555, 30.8024}, {96.5621, 98.4221, -1.86}};
+        return new Object[][]{{13.53, 5.12, 8.41}, {56.789874, 25.98744555, 30.8024},
+                {96.5621, 98.4221, -1.86}};
     }
 
     @DataProvider(name = "multi-long")
     public static Object[][] dataProviderMethodMultiLong() {
-        return new Object[][]{{5566L, 868L, 4831288L}, {98742L, -985214L, -97282000788L}, {12345679L, 9L, 111111111L}};
+        return new Object[][]{{5566L, 868L, 4831288L}, {98742L, -985214L, -97282000788L},
+                {12345679L, 9L, 111111111L}};
     }
 
     @DataProvider(name = "div-double")
